676e4bf48e6487be711dac45d1332bad53bfd1a2
deevite <ilyuha0794@gmail.com>
2025-06-25 15:55:01 +0600

    фикс открытия Telegram ссылки в новой вкладке

--- src/App.tsx
+++ src/App.tsx
@@ -216,7 +216,7 @@ function App() {
   };
 
   const handleApplicationClick = () => {
-    window.open('https://web.telegram.org/a/#1615820349', '_blank');
+    window.open(('https://t.me/BullTrendChannel', '_blank');
   };
 
   const scrollToDetails = (detailsType) => {
